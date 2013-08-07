440bd3b441bc840e68fb4e6b9864e38005e559fc
ayysir <dresadd09691@gmail.com>
2013-08-07 12:47:49 -0400

    Update cm-10.2-d2x.sh

--- cm-10.2-d2x.sh
+++ cm-10.2-d2x.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 
-# gralloc (display-caf)
-#CHANGES+=(46166)
-
 # DashPlayer: make jb_mr2 compatible
 CHANGES+=(46086)
 
-#Use display-caf for QCOM_BSP
-CHANGES+=(46183)
+# libgralloc:  Change ionFlags for GRALLOC_USAGE_PRIVATE_MM_HEAP case
+#   - fixes secure memory crash in OMX
+CHANGES+=(46521)
 
-# mm-video: revert usage of flag GRALLOC_USAGE_PRIVATE_CP_BUFFER
-CHANGES+=(46276)
+# qcom-common: enable hardware aac encoder
+CHANGES+=(47200)
 
 # Camera: Bring Samsung camera fixes and features to 4.3
 CHANGES+=(46287)
