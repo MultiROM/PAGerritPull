8d92a5fbee20d99aeef33a5c7e5d32a49e85fb15
ayysir <dresadd09691@gmail.com>
2013-08-09 16:57:04 -0400

    already sync'd

--- cm-10.2-d2x.sh
+++ cm-10.2-d2x.sh
@@ -7,10 +7,6 @@ CHANGES+=(46287)
 # audio-caf: Fix -Wreorder warnings
 CHANGES+=(47233)
 
-#d2-common:  Move Camera overlay settings to Gallery2
-# -Adjust settings for 10.2 camera
-CHANGES+=(46772)
-
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
 
