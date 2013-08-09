9cf52c3609a41d3181c03c371a9578fdb18839e8
ayysir <dresadd09691@gmail.com>
2013-08-09 14:17:02 -0400

    update

--- cm-10.2-d2x.sh
+++ cm-10.2-d2x.sh
@@ -8,6 +8,9 @@ CHANGES+=(46521)
 # Camera: Bring Samsung camera fixes and features to 4.3
 CHANGES+=(46287)
 
+# audio-caf: Fix -Wreorder warnings
+CHANGES+=(47233)
+
 #d2-common:  Move Camera overlay settings to Gallery2
 # -Adjust settings for 10.2 camera
 CHANGES+=(46772)
