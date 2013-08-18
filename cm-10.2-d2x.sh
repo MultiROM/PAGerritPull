ead608cbd650f77ab2c5a08e1778fb026bf77592
ayysir <dresadd09691@gmail.com>
2013-08-18 16:06:23 -0400

    Update

--- cm-10.2-d2x.sh
+++ cm-10.2-d2x.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-# audio-caf: Fix -Wreorder warnings
-CHANGES+=(47233)
+# Gallery2: Initial storage location configuration options
+CHANGES+=(47623)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
