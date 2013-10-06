7d1c1f822a174194f64d20280341e22af4b17d45
ayysir <dresadd09691@gmail.com>
2013-10-05 23:06:54 -0400

    Add option to turn off CRT effect
    Testing

--- jb43.sh
+++ jb43.sh
@@ -9,3 +9,10 @@
 # Below is an example on how to implent the gerrit changes
 # CHANGES+=()  inside the "()" is the ID number of the gerrit commit
 # i.e https://gerrit.paranoidandroid.co/#/c/1/ would be "CHANGES+=(1)"
+
+# fw/base: Add option to turn off CRT Effect [1/2]
+CHANGES+=(14)
+
+# Settings: Add option to turn off CRT Effect [2/2]
+CHANGES+=(15)
+
