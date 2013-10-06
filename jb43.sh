549f41d0e98b7e0817098e66fc296c599105e60b
ayysir <dresadd09691@gmail.com>
2013-10-06 00:08:14 -0400

    Switch

--- jb43.sh
+++ jb43.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-
-
-# Do the cherry-picking
-./build/tools/repopick.py -b ${CHANGES[@]}
-
-
 # Below is an example on how to implent the gerrit changes
 # CHANGES+=()  inside the "()" is the ID number of the gerrit commit
 # i.e https://gerrit.paranoidandroid.co/#/c/1/ would be "CHANGES+=(1)"
@@ -15,4 +9,12 @@ CHANGES+=(14)
 
 # Settings: Add option to turn off CRT Effect [2/2]
 CHANGES+=(15)
+
+
+
+# Do the cherry-picking
+./build/tools/repopick.py -b ${CHANGES[@]}
+
+
+
 
