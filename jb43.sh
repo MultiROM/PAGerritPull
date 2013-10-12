5f9b2af03a7141f8c9317acd4bc82ed393a7ab8b
ayysir <dresadd09691@gmail.com>
2013-10-12 01:41:16 -0400

    abandon: CRT off
    seems lack of interest for this so i'll just kill it

--- jb43.sh
+++ jb43.sh
@@ -4,11 +4,6 @@
 # CHANGES+=()  inside the "()" is the ID number of the gerrit commit
 # i.e https://gerrit.paranoidandroid.co/#/c/1/ would be "CHANGES+=(1)"
 
-# fw/base: Add option to turn off CRT Effect [1/2]
-CHANGES+=(14)
-
-# Settings: Add option to turn off CRT Effect [2/2]
-CHANGES+=(15)
 
 # Do the cherry-picking
 ./build/tools/repopick.py -b ${CHANGES[@]}
